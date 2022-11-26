b218b3db3b8a100643586dc153d6fd4d2c6b4a91
Fabiano Fregnani <99106631+fregnani123@users.noreply.github.com>
2022-11-26 13:23:05 -0300

    Nave

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+/*document.querySelector("#btnMobile").addEventListener("click", () => document.querySelector("#nav").classList.toggle("active"))*/
+
+
+
+let btn = document.querySelector("#btnMobile").addEventListener("click", toggleMenu); 
+
+function toggleMenu(){
+    const nav = document.querySelector("#nav")
+    nav.classList.toggle("active");
+}
+
+/*
+codigo: 3 
+let btn = document.querySelector("#btnMobile").addEventListener("click", toggleMenu); 
+
+let nav = document.querySelector("#nav");
+
+function toggleMenu() {
+if (nav.classList.contains("active")){
+ nav.classList.remove("active") 
+}else{nav.classList.add("active")}
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
